7e8ba24fbd512f1e3f6984c9f0655b74afef0a8e
Nikolay <halt.hammerzeit.at@gmail.com>
2016-07-20 20:28:40 +0300

    extracted style removal into `devtools`

--- code/page-server/web server.js
+++ code/page-server/web server.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import webpage_server from 'react-isomorphic-render/page-server'
-import common from '../client/react-isomorphic-render'
+import { devtools } from 'universal-webpack'
 
+import common from '../client/react-isomorphic-render'
 import Log from '../common/log'
 
 const log = Log('webpage renderer')
@@ -51,58 +52,11 @@ export default function(parameters)
 		// (its gonna work with or without this `head()` parameter)
 		head: () =>
 		{
-			// The following code is an advanced topic
-			// and can be skipped safely.
-			// This code is not required
-			// for the whole thing to work.
-			// It's gonna work fine without this code.
-			//
-			// (in development mode)
-			//
-			// Removing the now unnecessary `<link rel="stylesheet"/>` tag,
-			// because the client-side javascript has already kicked-in
-			// and added all the styles using `style-loader` dynamically.
-			//
-			// Should that stylesheet be removed at all?
-			// Not necessarily.
-			// It's just, for example, if a developer opens the page,
-			// then decides to remove some CSS class,
-			// and switches back to the browser,
-			// and the CSS class is still there,
-			// because it was only removed from dynamically added CSS styles,
-			// not the statically added ones on the server-side.
-			//
 			if (_development_)
 			{
-				const script =
-				`
-					document.addEventListener('DOMContentLoaded', function(event)
-					{
-						const stylesheets = document.querySelectorAll('head link[rel="stylesheet"]')
-
-						// If `style-loader` has already added <link/>s 
-						// to its dynamic hot-reloadable styles,
-						// then remove the <link/> to the static CSS bundle
-						// inserted during server side page rendering.
-						if (stylesheets.length > 1)
-						{
-							// Waits a "magic" time amount of 2 seconds
-							// for the dynamically added stylesheets
-							// to be parsed and applied to the page.
-							setTimeout(function()
-							{
-								stylesheets[0].parentNode.removeChild(stylesheets[0])
-							},
-							2000)
-						}
-						else
-						{
-							console.log("Couldn't remove server-rendered stylesheet tag because the dynamically added stylesheets haven't yet been added")
-						}
-					})
-				`
-
-				return <script dangerouslySetInnerHTML={{ __html: script }}/>
+				// `devtools` just tampers with CSS styles a bit.
+				// It's not required for operation and can be omitted.
+				return <script dangerouslySetInnerHTML={{ __html: devtools }}/>
 			}
 		}
 	},
