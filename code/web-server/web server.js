694139b788834434682680f3a5e20f9d10f079bf
Nikolay <halt.hammerzeit.at@gmail.com>
2016-08-02 22:01:39 +0300

    added proxy target names

--- code/web-server/web server.js
+++ code/web-server/web server.js
@@ -5,15 +5,15 @@ import web_service from 'web-service'
 const web = web_service({ log })
 
 // serve static files
-web.serve_static_files('/assets', path.join(Root_folder, 'build', 'assets'))
+web.files('/assets', path.join(Root_folder, 'build', 'assets'))
 
 // if it's not a static file url:
 
 // Proxy /api requests to API server
-web.proxy('/api', `http://${configuration.api_server.http.host}:${configuration.api_server.http.port}`)
+web.proxy('/api', `http://${configuration.api_server.http.host}:${configuration.api_server.http.port}`, { name: 'API service' })
 
 // Proxy all the rest requests to Webpage rendering server
-web.proxy(`http://${configuration.webpage_server.http.host}:${configuration.webpage_server.http.port}`)
+web.proxy(`http://${configuration.webpage_server.http.host}:${configuration.webpage_server.http.port}`, { name: 'Page rendering service' })
 
 // поднять http сервер
 web.listen(configuration.web_server.http.port).then(() =>
