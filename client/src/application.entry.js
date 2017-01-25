6db8f000ac7d3deb3f34f878137f458f285043c2
Nikolay <halt.hammerzeit.at@gmail.com>
2017-01-25 22:08:56 +0300

    bluebird options added

--- client/src/application.entry.js
+++ client/src/application.entry.js
@@ -1,9 +1,21 @@
 // use bluebird for Promises
-require('babel-runtime/core-js/promise').default = require('bluebird');
+require('babel-runtime/core-js/promise').default = require('bluebird')
+
+var bluebird_options = {
+  // Enable cancellation
+  cancellation: true
+}
 
 if (process.env.NODE_ENV !== 'production') {
-  require('bluebird').longStackTraces();
+  // Enable warnings
+  bluebird_options.warnings = true
+  // Enable long stack traces
+  bluebird_options.longStackTraces = true
+  // Enable monitoring
+  bluebird_options.monitoring = true
 }
+
+require('bluebird').config(bluebird_options)
 
 /**
  * Warning from React Router, caused by react-hot-loader.
@@ -12,16 +24,16 @@ if (process.env.NODE_ENV !== 'production') {
  * See https://github.com/gaearon/react-hot-loader/issues/298
  */
 if (module.hot) {
-  const isString = a => typeof a === 'string';
-  const orgError = console.error; // eslint-disable-line no-console
+  const isString = a => typeof a === 'string'
+  const orgError = console.error // eslint-disable-line no-console
   console.error = (...args) => { // eslint-disable-line no-console
     if (args && args.length === 1 && isString(args[0]) && args[0].indexOf('You cannot change <Router routes>;') > -1) {
       // React route changed
     } else {
       // Log the error as normally
-      orgError.apply(console, args);
+      orgError.apply(console, args)
     }
-  };
+  }
 }
 
-require('./application');
+require('./application')
