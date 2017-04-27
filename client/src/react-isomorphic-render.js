350c55275dab7264d6a8e223a0f55b8dd710d36c
Nikolay <halt.hammerzeit.at@gmail.com>
2017-04-27 20:55:31 +0300

    `preload.catch` => `catch`. Closes #29

--- client/src/react-isomorphic-render.js
+++ client/src/react-isomorphic-render.js
@@ -13,33 +13,30 @@ export default
 	routes,
 	wrapper,
 
-	preload:
+	catch(error, { path, url, redirect, dispatch, getState, server })
 	{
-		catch(error, { path, url, redirect, dispatch, getState, server })
+		console.error(`Error while preloading "${url}"`)
+		console.error(error)
+		
+		// // Not authenticated
+		// if (error.status === 401)
+		// {
+		// 	return redirect('/unauthenticated')
+		// }
+
+		// // Not authorized
+		// if (error.status === 403)
+		// {
+		// 	return redirect('/unauthorized')
+		// }
+
+		// Redirect to a generic error page
+		if (process.env.NODE_ENV === 'production')
 		{
-			console.error(`Error while preloading "${url}"`)
-			console.error(error)
-			
-			// // Not authenticated
-			// if (error.status === 401)
-			// {
-			// 	return redirect('/unauthenticated')
-			// }
-
-			// // Not authorized
-			// if (error.status === 403)
-			// {
-			// 	return redirect('/unauthorized')
-			// }
-
-			// Redirect to a generic error page
-			if (process.env.NODE_ENV === 'production')
-			{
-				redirect('/error')
-			}
-
-			throw error
+			redirect('/error')
 		}
+
+		throw error
 	},
 
 	...asyncSettings
