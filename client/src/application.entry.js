a0bbd8735ef6b4718079ba4481bd87e689882be2
Nikolay <halt.hammerzeit.at@gmail.com>
2017-09-29 04:34:06 +0300

    babel-preset-env

--- client/src/application.entry.js
+++ client/src/application.entry.js
@@ -9,13 +9,13 @@ require('../../bluebird')
  */
 if (module.hot) {
   const isString = a => typeof a === 'string'
-  const orgError = console.error // eslint-disable-line no-console
+  const consoleError = console.error // eslint-disable-line no-console
   console.error = (...args) => { // eslint-disable-line no-console
     if (args && args.length === 1 && isString(args[0]) && args[0].indexOf('You cannot change <Router routes>;') > -1) {
       // React route changed
     } else {
       // Log the error as normally
-      orgError.apply(console, args)
+      consoleError.apply(console, args)
     }
   }
 }
