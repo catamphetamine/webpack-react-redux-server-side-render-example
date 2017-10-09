cb232b41253a65455277ecdc1170b96490008e2e
Nikolay <halt.hammerzeit.at@gmail.com>
2017-10-09 15:09:29 +0300

    misc

--- client/src/react-isomorphic-render-async.js
+++ client/src/react-isomorphic-render-async.js
@@ -5,14 +5,14 @@ export default {
   // as part of an asynchronous Redux action
   // this will generate `events` from `event`
   // using this function.
-  asynchronousActionEventNaming: event => ([
+  reduxEventNaming: (event) => ([
     `${event}_PENDING`,
     `${event}_SUCCESS`,
     `${event}_ERROR`
   ]),
 
-  // When using `asynchronousActionHandler`
+  // When using "redux module" feature
   // this function will generate a Redux state property name from an event name.
   // E.g. event `GET_USERS_ERROR` => state.`getUsersError`.
-  asynchronousActionHandlerStatePropertyNaming: underscoredToCamelCase
+  reduxPropertyNaming: underscoredToCamelCase
 };
