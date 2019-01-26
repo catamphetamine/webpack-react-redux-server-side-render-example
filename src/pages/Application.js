5f7c018a2a6e0554e05edbf95c0c8f001da9eef2
catamphetamine <purecatamphetamine@gmail.com>
2019-01-26 18:57:48 +0300

    misc unrelated

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -11,7 +11,7 @@ import 'react-website/components/Loading.css'
 // `react-time-ago` English language.
 import JavascriptTimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
-JavascriptTimeAgo.locale(en)
+JavascriptTimeAgo.addLocale(en)
 
 import Menu, { MenuLink } from '../components/Menu'
 import Snackbar from '../components/Snackbar'
