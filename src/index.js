c4e754145150702e3b059d31120dabb1ee75816b
catamphetamine <purecatamphetamine@gmail.com>
2019-10-04 01:41:28 +0300

    require('react-hot-loader')

--- src/index.js
+++ src/index.js
@@ -6,5 +6,9 @@ require('regenerator-runtime/runtime')
 // Maintain CSS styles order.
 require('./styles/style.css')
 
+// https://github.com/gaearon/react-hot-loader
+// "Make sure `react-hot-loader` is required before `react` and `react-dom`".
+require('react-hot-loader')
+
 // Run the application.
 require('./render').default().catch((error) => console.error(error))
